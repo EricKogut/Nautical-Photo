56f902b2ea3893b81fc3982486b677c821fb3c8f
EricKogut <eric.jkogut@gmail.com>
2021-09-15 19:28:34 -0400

    Adding explore, landing page, routes
    - adding routing between the different things too :)

--- NauticalPhotoAngular/src/app/app.module.ts
+++ NauticalPhotoAngular/src/app/app.module.ts
@@ -4,15 +4,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+/* Custom Components */
+import { LandingComponent } from './_Pages/landing/landing.component';
+import { ExploreComponent } from './_Pages/explore/explore.component';
+
+/* Added components */
+import { Routes, RouterModule } from '@angular/router';
+
+const myRoutes: Routes = [
+  { path: '', component: LandingComponent },
+  { path: 'explore', component: ExploreComponent },
+];
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, LandingComponent, ExploreComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(myRoutes, { useHash: true }),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
